7f6b3be88fdc09cfe82336bf7579520861def46d
EthanMui <fpsmuie48@stu.fpsk12.net>
2020-11-12 20:10:59 -0500

    ethan

--- SimpleStory/src/main/java/frc/robot/Robot.java
+++ SimpleStory/src/main/java/frc/robot/Robot.java
@@ -14,7 +14,8 @@ import edu.wpi.first.wpilibj.PWMVictorSPX;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import frc.robot.storyparts.MrZStoryPart;
+import frc.robot.storyparts.EthanStoryPart;
+
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -44,7 +45,7 @@ public class Robot extends TimedRobot {
    */
   private void buildStoryParts() {
     // make sure we add the story parts to our ArrayList
-    storyParts.add(new MrZStoryPart());
+    storyParts.add(new EthanStoryPart());
   }
 
   /**
